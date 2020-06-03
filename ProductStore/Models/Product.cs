cad79030e95a1eb1e98b3bc89a8c0d57feb59785
enkr1 <enkr99@gmail.com>
2020-06-03 11:36:04 +0800

    first init

--- ProductStore/Models/Product.cs
+++ ProductStore/Models/Product.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations; // Practical 3
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,9 +9,12 @@ namespace ProductStore.Models
     public class Product
     {
         public int Id { get; set; }
+
         [Required]
         public string Name { get; set; }
+
         public string Category { get; set; }
+
         [Required]
         [Range(0, 100)]
         public decimal Price { get; set; }
